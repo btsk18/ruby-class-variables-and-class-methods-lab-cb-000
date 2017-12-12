3e42708289e5fa16d02211a2ba1f2d46019c9e48
Brandon Leeds <brandonleeds@gmail.com>
2017-12-12 00:00:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@genres = []
   @@artists = []
 
-  def initializer(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
